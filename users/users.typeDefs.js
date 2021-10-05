c0d0ee76396afd46caac00fba391791d9d852984
yooja <wogud758787@naver.com>
2021-10-05 17:55:07 +0900

    4.0

---
+++ users/users.typeDefs.js
@@ -0,0 +1,23 @@
+export default gql`
+	type User{
+		id:			Int!
+		firstName:	String!
+		lastName:	String
+		username:	String!
+		email:		String!
+		createdAt:	String!
+		updatedAt:	String!
+	}
+	type Mutation {
+		createAccount(
+			firstName:	String!
+			lastName:	String
+			username:	String!
+			email:		String!
+			password:	String!
+		):User
+	}
+	type Query {
+		seeProfile(username:String):User
+	}
+`;
